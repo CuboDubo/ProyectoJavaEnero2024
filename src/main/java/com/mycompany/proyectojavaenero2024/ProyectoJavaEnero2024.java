cdff200b93c77f13595f2c4180229d43efffc38f
CuboDubo <152566737+CuboDubo@users.noreply.github.com>
2024-02-12 08:59:43 +0100

    Update ProyectoJavaEnero2024.java

--- src/main/java/com/mycompany/proyectojavaenero2024/ProyectoJavaEnero2024.java
+++ src/main/java/com/mycompany/proyectojavaenero2024/ProyectoJavaEnero2024.java
@@ -4,5 +4,6 @@ public class ProyectoJavaEnero2024 {
 
     public static void main(String[] args) {
         System.out.println("Esto es un control de versiones en NetBeans");
+        System.out.println("Ya estamos en el último punto");
     }
 }
